d6b428ed771e5d90a5a2b3ba7c00625928c967af
kamrancouture <65361792+kamrancouture@users.noreply.github.com>
2023-12-14 08:23:08 -0800

    enemy detector

--- level_1.gd
+++ level_1.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
+onready var player = get_node("player")
 onready var tiles = get_node("tiles")
 onready var enemies = get_node("enemies")
 
 func _physics_process(delta):
+	
+	$enemy_detector/CollisionShape2D.global_position = player.global_position
+	
 	if Global.lights_out:
 		tiles.show()
 		enemies.hide()
@@ -16,3 +20,7 @@ func _physics_process(delta):
 
 func _on_final_light_switch_body_entered(body):
 	get_tree().change_scene("res://StartMenu.tscn")
+
+
+func _on_enemy_detector_body_entered(body):
+	body.attacking = true
